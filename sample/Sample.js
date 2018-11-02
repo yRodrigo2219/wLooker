304813bf9b4ea38b9177eb30a16fe496023691b3
Wandalen <wandalen.me@gmail.com>
2018-11-02 12:35:17 +0200

    auto

--- sample/Sample.js
+++ sample/Sample.js
@@ -1,7 +1,22 @@
 
-let _ = require( 'wTools' );
+let _ = require( 'wLooker' )
 
 /**/
 
-...
+let structure =
+{
+  a : 1,
+  b : 's',
+  c : [ 1,3 ],
+  d : [ 1,{ date : new Date() } ],
+  e : function(){},
+  f : new ArrayBuffer( 13 ),
+  g : new Float32Array([ 1,2,3 ]),
+}
 
+function handleUp( e, k, it )
+{
+  console.log( it.path );
+}
+
+_.look( structure, handleUp );
