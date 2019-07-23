1a384d9f4d4c2ba5f459d72a8b21ee520119a3c9
Wandalen <wandalen.me@gmail.com>
2019-07-23 06:43:03 +0300

    auto commit

--- proto/dwtools/abase/l3/Looker.s
+++ proto/dwtools/abase/l3/Looker.s
@@ -918,10 +918,6 @@ _.mapSupplement( Self, Supplement );
 // export
 // --
 
-// if( typeof module !== 'undefined' )
-// if( _global_.WTOOLS_PRIVATE )
-// { /* delete require.cache[ module.id ]; */ }
-
 if( typeof module !== 'undefined' && module !== null )
 module[ 'exports' ] = Self;
 
